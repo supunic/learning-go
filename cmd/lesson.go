ae70754df3f164b801c38eebd936f5343924cd1c
seiya endo <seiya.endo0830@gmail.com>
2021-11-07 22:52:38 +0900

    [update] channel producer & consumer

--- cmd/lesson.go
+++ cmd/lesson.go
@@ -1,21 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-func goroutine1(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-		c <- sum
+func producer(ch chan int, i int) {
+	// something
+	ch <- i * 2
+}
+
+func consumer(ch chan int, wg *sync.WaitGroup) {
+	// producerでchに値が挿入される度に反応
+	// ch がcloseするまで待機中のイメージ
+	for i := range ch {
+		// deferでwg.Done()を定義させるためにインナー関数にする
+		// -> 処理の途中でエラーになった場合でもwg.Done()が実行されるように
+		func() {
+			defer wg.Done()
+			fmt.Println("process", i*1000)
+		}()
 	}
-	close(c)
+	fmt.Println("################")
 }
 
 func main() {
-	s := []int{1, 2, 3, 4, 5}
-	c := make(chan int, len(s))
-	go goroutine1(s, c)
-	for i := range c {
-		fmt.Println(i)
+	var wg sync.WaitGroup
+	ch := make(chan int)
+
+	// Producer（chに挿入）
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go producer(ch, i)
 	}
+
+	// Consumer（chの中身を処理）
+	go consumer(ch, &wg)
+	wg.Wait()
+	close(ch) // consumer関数のrange chを終了させる
+	time.Sleep(2 * time.Second)
+	fmt.Println("Done")
 }
