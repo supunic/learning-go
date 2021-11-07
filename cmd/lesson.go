8ef2d46df12daf7ad9b8d16e90a42b44f91dccec
seiya endo <seiya.endo0830@gmail.com>
2021-11-07 15:18:48 +0900

    [update] range

--- cmd/lesson.go
+++ cmd/lesson.go
@@ -3,28 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 10; i++ {
-		if i == 3 {
-			fmt.Println("continue")
-			continue
-		}
+	l := []string{"python", "go", "java"}
 
-		if i > 5 {
-			break
-		}
-		fmt.Println(i)
+	for i := 0; i < len(l); i++ {
+		fmt.Println(i, l[i])
 	}
 
-	sum := 1
-	for sum < 10 {
-		sum += sum
-		fmt.Println(sum)
+	for i, v := range l {
+		fmt.Println(i, v)
+	}
+	for _, v := range l {
+		fmt.Println(v)
 	}
 
-	/*
-		// 無限ループ
-		for {
-			fmt.Println("hello")
-		}
-	*/
+	m := map[string]int{"apple": 100, "banana": 200}
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+	for k := range m {
+		fmt.Println(k)
+	}
+	for _, v := range m {
+		fmt.Println(v)
+	}
+
 }
