fa924695b677fc70012890cbbdf34abdb8cc401e
seiya endo <seiya.endo0830@gmail.com>
2021-11-07 23:01:46 +0900

    [update] channel fan-out fan-in

--- cmd/lesson.go
+++ cmd/lesson.go
@@ -1,44 +1,38 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
-func producer(ch chan int, i int) {
-	// something
-	ch <- i * 2
+func producer(first chan<- int) {
+	defer close(first)
+	for i := 0; i < 10; i++ {
+		first <- i
+	}
 }
 
-func consumer(ch chan int, wg *sync.WaitGroup) {
-	// producerでchに値が挿入される度に反応
-	// ch がcloseするまで待機中のイメージ
-	for i := range ch {
-		// deferでwg.Done()を定義させるためにインナー関数にする
-		// -> 処理の途中でエラーになった場合でもwg.Done()が実行されるように
-		func() {
-			defer wg.Done()
-			fmt.Println("process", i*1000)
-		}()
+func multi2(first <-chan int, second chan<- int) {
+	defer close(second)
+	for i := range first {
+		second <- i * 2
 	}
-	fmt.Println("################")
+}
+
+func multi4(second <-chan int, third chan<- int) {
+	defer close(third)
+	for i := range second {
+		third <- i * 4
+	}
 }
 
 func main() {
-	var wg sync.WaitGroup
-	ch := make(chan int)
+	first := make(chan int)
+	second := make(chan int)
+	third := make(chan int)
 
-	// Producer（chに挿入）
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go producer(ch, i)
+	go producer(first)
+	go multi2(first, second)
+	go multi4(second, third)
+
+	for result := range third {
+		fmt.Println(result)
 	}
-
-	// Consumer（chの中身を処理）
-	go consumer(ch, &wg)
-	wg.Wait()
-	close(ch) // consumer関数のrange chを終了させる
-	time.Sleep(2 * time.Second)
-	fmt.Println("Done")
 }
