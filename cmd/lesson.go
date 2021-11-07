c3e1c957714cd86381b6b7a00d4630198ec3ca45
seiya endo <seiya.endo0830@gmail.com>
2021-11-07 16:01:35 +0900

    [update] error handling

--- cmd/lesson.go
+++ cmd/lesson.go
@@ -2,33 +2,24 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
 )
 
-func LoggingSettings(logFile string) {
-	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	multiLogFile := io.MultiWriter(os.Stdout, logfile)
-	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
-	log.SetOutput(multiLogFile)
-}
-
 func main() {
-	// doc
-	// https://pkg.go.dev/log
-
-	LoggingSettings("test.log")
-	_, err := os.Open("fafafa")
+	file, err := os.Open("./lesson.go")
 	if err != nil {
-		log.Fatalln("Exit", err) // プログラム終了
+		log.Fatalln("Error", err)
 	}
+	defer file.Close()
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatalln("Error", err)
+	}
+	fmt.Println(count, string(data))
 
-	log.Println("logging!")
-	log.Printf("%T %v", "test", "test")
-
-	log.Fatalf("%T %v", "test", "test")
-	log.Fatalln("error!!") // プログラム終了
-
-	fmt.Println("ok!")
+	if err = os.Chdir("test"); err != nil {
+		log.Fatalln("Error", err)
+	}
 }
