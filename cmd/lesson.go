e3d6e2ebc304a76ce6f49a6c386c481404bbd689
seiya endo <seiya.endo0830@gmail.com>
2021-11-07 11:59:47 +0900

    [update] slice

--- cmd/lesson.go
+++ cmd/lesson.go
@@ -3,13 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var a [2]int
-	a[0] = 100
-	a[1] = 200
-	fmt.Println(a)
+	n := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(n)
+	fmt.Println(n[2])   // 3
+	fmt.Println(n[2:4]) // [3, 4]
+	fmt.Println(n[:2])  // [1, 2]
+	fmt.Println(n[2:])  // [3, 4, 5, 6]
 
-	var b = [2]int{100, 200}
-	// 配列はサイズ変更できない
-	//b = append(b, 300)
-	fmt.Println(b)
+	n[2] = 100
+	fmt.Println(n)
+
+	// スライス in スライス
+	var board = [][]int{
+		{0, 1, 2},
+		{3, 4, 5},
+		{6, 7, 8},
+	}
+	fmt.Println(board)
+
+	n = append(n, 100, 200, 300, 400)
+	fmt.Println(n)
 }
