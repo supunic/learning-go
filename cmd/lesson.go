cad6a105bac48310bc61c0c28805bcaac31475bb
seiya endo <seiya.endo0830@gmail.com>
2021-11-07 23:12:17 +0900

    [update] channel select

--- cmd/lesson.go
+++ cmd/lesson.go
@@ -1,38 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func producer(first chan<- int) {
-	defer close(first)
-	for i := 0; i < 10; i++ {
-		first <- i
+func goroutine1(ch chan string) {
+	for {
+		ch <- "packet from 1"
+		time.Sleep(1 * time.Second)
 	}
 }
 
-func multi2(first <-chan int, second chan<- int) {
-	defer close(second)
-	for i := range first {
-		second <- i * 2
-	}
-}
-
-func multi4(second <-chan int, third chan<- int) {
-	defer close(third)
-	for i := range second {
-		third <- i * 4
+func goroutine2(ch chan int) {
+	for {
+		ch <- 100
+		time.Sleep(2 * time.Second)
 	}
 }
 
 func main() {
-	first := make(chan int)
-	second := make(chan int)
-	third := make(chan int)
+	c1 := make(chan string)
+	c2 := make(chan int)
 
-	go producer(first)
-	go multi2(first, second)
-	go multi4(second, third)
+	go goroutine1(c1)
+	go goroutine2(c2)
 
-	for result := range third {
-		fmt.Println(result)
+	for {
+		select {
+		case msg1 := <-c1:
+			fmt.Println(msg1)
+		case msg2 := <-c2:
+			fmt.Println(msg2)
+		}
 	}
 }
