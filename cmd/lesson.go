fc695013fbee6ff61c76d900bb8ec405e45e90f1
seiya endo <seiya.endo0830@gmail.com>
2021-11-07 15:32:18 +0900

    [update] defer

--- cmd/lesson.go
+++ cmd/lesson.go
@@ -2,30 +2,41 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"os"
 )
 
-func getOsName() string {
-	return "mac"
+func foo() {
+	defer fmt.Println("world foo")
+	fmt.Println("hello foo")
 }
 
 func main() {
-	switch os := getOsName(); os {
-	case "mac":
-		fmt.Println("Mac!")
-	case "windows":
-		fmt.Println("Windows!")
-	default:
-		fmt.Println("Default!", os)
-	}
+	// 遅延実行 -> main関数が終了した後に実行される
+	/*
+		defer fmt.Println("world")
+		foo()
+		fmt.Println("hello")
+		// hello
+		// world
+	*/
 
-	t := time.Now()
-	fmt.Println(t.Hour())
+	/*
+		// stacking defer
+		fmt.Println("run")
+		defer fmt.Println(1)
+		defer fmt.Println(2)
+		defer fmt.Println(3)
+		fmt.Println("success")
+		// run
+		// success
+		// 3
+		// 2
+		// 1
+	*/
 
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("Morning")
-	case t.Hour() < 17:
-		fmt.Println("Afternoon")
-	}
+	file, _ := os.Open("./lesson.go")
+	defer file.Close()        // deferで忘れずにfileをclose
+	data := make([]byte, 100) // バイト配列
+	file.Read(data)           // バイト配列にデータを入れる
+	fmt.Println(string(data)) // stringにcast
 }
