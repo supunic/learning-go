774271cf772bfc1ddcc13de2665f93ef482db636
seiya endo <seiya.endo0830@gmail.com>
2021-11-07 11:52:59 +0900

    [update] array

--- cmd/lesson.go
+++ cmd/lesson.go
@@ -1,28 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 func main() {
-	// int -> float
-	var x = 1
-	xx := float64(1)
-	fmt.Printf("%T %v %f\n", x, x, x)
-	fmt.Printf("%T %v %f\n", xx, xx, xx)
+	var a [2]int
+	a[0] = 100
+	a[1] = 200
+	fmt.Println(a)
 
-	// float -> int
-	var y = 1.2
-	yy := int(y)
-	fmt.Printf("%T %v %d\n", yy, yy, yy)
-
-	// string -> int
-	var s = "14"
-	//z := int(s) // できない
-	i, err := strconv.Atoi(s)
-	if err != nil {
-		fmt.Println("ERROR")
-	}
-	fmt.Printf("%T %v %d\n", i, i, i)
+	var b = [2]int{100, 200}
+	// 配列はサイズ変更できない
+	//b = append(b, 300)
+	fmt.Println(b)
 }
