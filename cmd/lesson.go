ad0071d8f2fb1bb70709d91b801488107c965cb2
seiya endo <seiya.endo0830@gmail.com>
2021-11-07 22:26:07 +0900

    [update] goroutine buffered channels

--- cmd/lesson.go
+++ cmd/lesson.go
@@ -2,29 +2,26 @@ package main
 
 import "fmt"
 
-func goroutine1(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	c <- sum
-}
-
-func goroutine2(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	c <- sum
-}
-
 func main() {
-	s := []int{1, 2, 3, 4, 5}
-	c := make(chan int)
-	go goroutine1(s, c)
-	go goroutine2(s, c)
-	x := <-c // ブロッキングされてgoroutine1が終わるまで進まない
-	fmt.Println(x)
-	y := <-c // ブロッキングされてgoroutine2が終わるまで進まない
-	fmt.Println(y)
+	ch := make(chan int, 2)
+	ch <- 100
+	fmt.Println(len(ch))
+	ch <- 200
+	fmt.Println(len(ch))
+
+	/*
+		// channelから1つとり出す
+		x := <-ch
+		fmt.Println(x)
+		fmt.Println(len(ch))
+
+		ch <- 300
+		fmt.Println(len(ch))
+	*/
+
+	close(ch) // channelをcloseして終了させる
+
+	for c := range ch {
+		fmt.Println(c)
+	}
 }
