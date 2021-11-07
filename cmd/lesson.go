c29f3aafb05b7089c61e7530d8866e520225ca31
seiya endo <seiya.endo0830@gmail.com>
2021-11-07 10:56:05 +0900

    [update] const

--- cmd/lesson.go
+++ cmd/lesson.go
@@ -2,27 +2,13 @@ package main
 
 import "fmt"
 
-var (
-	i    = 1
-	f64  = 1.2
-	s    = "test"
-	t, f = true, false
+const Pi = 3.14
+
+const (
+	UserName = "test_user"
+	Password = "test_pass"
 )
 
-func foo() {
-	// short valuable declarations
-	// -> 関数内のみ
-	xi := 1                // := で宣言と代入、型を自動で割り振り
-	xf64 := 1.2            // デフォルトでfloat64になる
-	var xf32 float32 = 1.2 // float32に指定した場合
-	xs := "test"
-	xt, xf := true, false
-	fmt.Println(xi, xf64, xs, xt, xf)
-	fmt.Printf("%T\n", xf64)
-	fmt.Printf("%T\n", xf32)
-}
-
 func main() {
-	fmt.Println(i, f64, s, t, f)
-	foo()
+	fmt.Println(Pi, UserName, Password)
 }
