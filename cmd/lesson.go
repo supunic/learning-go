23dbd4dccf88d0764e27f8f729c4e3e035604d3d
seiya endo <seiya.endo0830@gmail.com>
2021-11-07 19:00:13 +0900

    [update] goroutine

--- cmd/lesson.go
+++ cmd/lesson.go
@@ -1,31 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-type UserNotFound struct {
-	Username string
-}
-
-func (e *UserNotFound) Error() string {
-	// eはポインタで受け取るのが良しとされている
-	// -> エラー比較時に区別させるため
-	return fmt.Sprintf("User not found: %v", e.Username)
-}
-
-func myFunc() error {
-	ok := false // something wrong ...
-	if ok {
-		return nil // 本来だとerrorなし
+func goroutine(s string, wg *sync.WaitGroup) {
+	defer wg.Done() // goroutineの終了の宣言
+	for i := 0; i < 5; i++ {
+		fmt.Println(s)
 	}
-	// &UserNotFound &つける
-	return &UserNotFound{Username: "mike"}
+}
+
+func normal(s string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println(s)
+	}
 }
 
 func main() {
-	// カスタムエラー
-	// -> 自分で定義したエラーを出せる
-	//    structを自分で用意して、そのstructにError()メソッドを追加する
-	if err := myFunc(); err != nil {
-		fmt.Println(err)
-	}
+	// sync.WaitGroup ... goroutineの制御に使用する
+	var wg sync.WaitGroup
+	defer wg.Wait() // goroutine待機の宣言
+
+	wg.Add(1)                  // goroutine追加
+	go goroutine("world", &wg) // goroutine実行
+
+	normal("hello")
 }
