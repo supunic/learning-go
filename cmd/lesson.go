3bf834b7098f93c429a240c7ac10925227192820
seiya endo <seiya.endo0830@gmail.com>
2021-11-07 22:33:09 +0900

    [update] channel range & close

--- cmd/lesson.go
+++ cmd/lesson.go
@@ -2,26 +2,20 @@ package main
 
 import "fmt"
 
+func goroutine1(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+		c <- sum
+	}
+	close(c)
+}
+
 func main() {
-	ch := make(chan int, 2)
-	ch <- 100
-	fmt.Println(len(ch))
-	ch <- 200
-	fmt.Println(len(ch))
-
-	/*
-		// channelから1つとり出す
-		x := <-ch
-		fmt.Println(x)
-		fmt.Println(len(ch))
-
-		ch <- 300
-		fmt.Println(len(ch))
-	*/
-
-	close(ch) // channelをcloseして終了させる
-
-	for c := range ch {
-		fmt.Println(c)
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int, len(s))
+	go goroutine1(s, c)
+	for i := range c {
+		fmt.Println(i)
 	}
 }
