9afa0cfa1634f27d097820bde620e46b5097504b
seiya endo <seiya.endo0830@gmail.com>
2021-11-07 16:50:30 +0900

    [update] struct

--- cmd/lesson.go
+++ cmd/lesson.go
@@ -2,33 +2,52 @@ package main
 
 import "fmt"
 
+type Vertex struct {
+	// 大文字はpublic
+	X, Y int
+	S    string
+	// 小文字はprivateの意
+	x int
+}
+
+func changeVertex(v Vertex) {
+	v.X = 1000
+}
+
+func changeVertex2(v *Vertex) {
+	v.X = 1000 // structの場合*を付けなくても実態にアクセスされる
+}
+
 func main() {
-	s := make([]int, 0)
-	fmt.Printf("%T %v\n", s, s)
+	vv := Vertex{X: 1, Y: 2}
+	changeVertex(vv)
+	fmt.Println(vv) // 書き変わらない
+	// {1 2  0}
 
-	m := make(map[string]int)
-	fmt.Printf("%T %v\n", m, m)
+	vv2 := &Vertex{X: 1, Y: 2}
+	changeVertex2(vv2)
+	fmt.Println(*vv2) // 書き換わる
+	// {1000 2  0}
 
-	var p = new(int) // メモリの領域を確保する
-	fmt.Printf("%T %v\n", p, p)
+	v := Vertex{X: 1, Y: 2}
+	fmt.Println(v)
+	fmt.Println(v.X, v.Y)
 
-	fmt.Println(p)  // 0xc000018100
-	fmt.Println(*p) // 0
-	*p++
-	fmt.Println(*p) // 1
+	v2 := Vertex{X: 1}
+	fmt.Println(v2)
 
-	ch := make(chan int)
-	fmt.Printf("%T\n%v\n", ch, ch)
+	v3 := Vertex{1, 2, "test", 1}
+	fmt.Println(v3)
 
-	st := new(struct{})
-	fmt.Printf("%T\n%v\n", st, st)
+	v4 := Vertex{}
+	fmt.Println(v4)
 
-	// makeもnewもメモリの領域を確保する
-	// make -> 値を返す
-	// new -> ポインタを返す
+	var v5 Vertex
+	fmt.Println(v5) // スライスやマップはnilだがstructは0
 
-	/*
-		var p2 *int
-		fmt.Println(p2) // <nil>
-	*/
+	v6 := new(Vertex)
+	fmt.Println(v6) // これはポインタが返る
+
+	v7 := &Vertex{}
+	fmt.Println(v7) // これもポインタが返る
 }
