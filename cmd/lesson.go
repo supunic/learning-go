e92fc008002d32f9d5d40cc23d44834ff6f6a13d
seiya endo <seiya.endo0830@gmail.com>
2021-11-07 15:23:27 +0900

    [update] switch

--- cmd/lesson.go
+++ cmd/lesson.go
@@ -1,31 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func getOsName() string {
+	return "mac"
+}
 
 func main() {
-	l := []string{"python", "go", "java"}
-
-	for i := 0; i < len(l); i++ {
-		fmt.Println(i, l[i])
+	switch os := getOsName(); os {
+	case "mac":
+		fmt.Println("Mac!")
+	case "windows":
+		fmt.Println("Windows!")
+	default:
+		fmt.Println("Default!", os)
 	}
 
-	for i, v := range l {
-		fmt.Println(i, v)
-	}
-	for _, v := range l {
-		fmt.Println(v)
-	}
+	t := time.Now()
+	fmt.Println(t.Hour())
 
-	m := map[string]int{"apple": 100, "banana": 200}
-
-	for k, v := range m {
-		fmt.Println(k, v)
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Morning")
+	case t.Hour() < 17:
+		fmt.Println("Afternoon")
 	}
-	for k := range m {
-		fmt.Println(k)
-	}
-	for _, v := range m {
-		fmt.Println(v)
-	}
-
 }
