a6e5a0b5aea63590baa303d1dae4968ca0cce375
seiya endo <seiya.endo0830@gmail.com>
2021-12-08 14:51:27 +0900

    [update] sort

--- app/main.go
+++ app/main.go
@@ -2,24 +2,26 @@ package main
 
 import (
 	"fmt"
-	"regexp"
+	"sort"
 )
 
 func main() {
-	match, _ := regexp.MatchString("a([a-z]+)e", "apple")
-	fmt.Println(match)
-
-	r := regexp.MustCompile("a([a-z]+)e")
-	ms := r.MatchString("apple")
-	fmt.Println(ms)
-
-	r2 := regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
-	fs := r2.FindString("/view/test")
-	fmt.Println(fs)
-	fss := r2.FindStringSubmatch("/view/test")
-	fmt.Println(fss, fss[0], fss[1], fss[2])
-	fss = r2.FindStringSubmatch("/edit/test")
-	fmt.Println(fss, fss[0], fss[1], fss[2])
-	fss = r2.FindStringSubmatch("/save/test")
-	fmt.Println(fss, fss[0], fss[1], fss[2])
+	i := []int{5, 3, 2, 8, 7}
+	s := []string{"d", "a", "f"}
+	p := []struct {
+		Name string
+		Age  int
+	}{
+		{"Nancy", 20},
+		{"Vera", 40},
+		{"Mike", 30},
+		{"Bob", 50},
+	}
+	fmt.Println(i, s, p)
+	sort.Ints(i)
+	fmt.Println(i)
+	sort.Strings(s)
+	fmt.Println(s)
+	sort.Slice(p, func(i, j int) bool { return p[i].Name < p[j].Name })
+	fmt.Println(p)
 }
