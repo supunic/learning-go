73246301ce1dba3297cbd1ae0eda6e2c3ee717ec
seiya endo <seiya.endo0830@gmail.com>
2021-12-08 17:13:35 +0900

    [update] semaphore

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM golang:1.17.2-alpine
 
 RUN apk add --update && apk add git
 
-RUN mkdir /go/src/app
+RUN mkdir -p /go/src/app
 
 WORKDIR /go/src/app
--- app/main.go
+++ app/main.go
@@ -1,34 +1,37 @@
 package main
 
 import (
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/hex"
+	"context"
 	"fmt"
+	"golang.org/x/sync/semaphore"
+	"time"
 )
 
-var DB = map[string]string{
-	"User1Key": "User1Secret",
-	"User2Key": "User2Secret",
-}
+var s = semaphore.NewWeighted(1)
 
-func Server(apiKey, sign string, data []byte) {
-	apiSecret := DB[apiKey]
-	h := hmac.New(sha256.New, []byte(apiSecret))
-	h.Write(data)
-	expectedHMAC := hex.EncodeToString(h.Sum(nil))
-	fmt.Println(sign == expectedHMAC)
+func longProcess(ctx context.Context) {
+	// TryAcquire ... 他のgoroutineを待たずに終了
+	if isAcquire := s.TryAcquire(1); !isAcquire {
+		fmt.Println("Could not get lock.")
+		return
+	}
+	// Acquire ... 他のgoroutineを待ってから実行
+	//if err := s.Acquire(ctx, 1); err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	defer s.Release(1)
+	fmt.Println("Wait...")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Done")
 }
 
 func main() {
-	const apiKey = "User1Key"
-	const apiSecret = "User1Secret"
-
-	data := []byte("data")
-	h := hmac.New(sha256.New, []byte(apiSecret))
-	h.Write(data)
-	sign := hex.EncodeToString(h.Sum(nil))
-	fmt.Println(sign)
-
-	Server(apiKey, sign, data)
+	ctx := context.TODO()
+	go longProcess(ctx)
+	go longProcess(ctx)
+	go longProcess(ctx)
+	time.Sleep(2 * time.Second)
+	go longProcess(ctx)
+	time.Sleep(5 * time.Second)
 }
