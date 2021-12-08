833c4910effb3a4bc283b8de28d098856ae7c203
seiya endo <seiya.endo0830@gmail.com>
2021-12-08 16:48:43 +0900

    [update] hmac api Authentication

--- app/main.go
+++ app/main.go
@@ -1,51 +1,34 @@
 package main
 
 import (
-	"encoding/json"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 )
 
-type T struct{}
-
-type Person struct {
-	Name      string   `json:"name"`
-	Age       int      `json:"age,string"`
-	Nicknames []string `json:"nicknames,omitempty"`
-	T         *T       `json:"T,omitempty"`
+var DB = map[string]string{
+	"User1Key": "User1Secret",
+	"User2Key": "User2Secret",
 }
 
-// UnmarshalJSON 指定したstructに対してjson.Unmarshalしたときに使われる
-func (p *Person) UnmarshalJSON(b []byte) error {
-	type Person2 struct {
-		Name string
-	}
-	var p2 Person2
-	err := json.Unmarshal(b, &p2)
-	if err != nil {
-		fmt.Println(err)
-	}
-	p.Name = p2.Name + "!"
-	return err
-}
-
-// MarshalJSON 指定したstructに対してjson.Marshalしたときに使われる
-func (p Person) MarshalJSON() ([]byte, error) {
-	v, err := json.Marshal(&struct {
-		Name string
-	}{
-		Name: "Mr." + p.Name,
-	})
-	return v, err
+func Server(apiKey, sign string, data []byte) {
+	apiSecret := DB[apiKey]
+	h := hmac.New(sha256.New, []byte(apiSecret))
+	h.Write(data)
+	expectedHMAC := hex.EncodeToString(h.Sum(nil))
+	fmt.Println(sign == expectedHMAC)
 }
 
 func main() {
-	b := []byte(`{"name": "mike", "age": "20", "nicknames": ["a", "b", "c"]}`)
-	var p Person
-	if err := json.Unmarshal(b, &p); err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(p.Name, p.Age, p.Nicknames)
+	const apiKey = "User1Key"
+	const apiSecret = "User1Secret"
 
-	v, _ := json.Marshal(p)
-	fmt.Println(string(v))
+	data := []byte("data")
+	h := hmac.New(sha256.New, []byte(apiSecret))
+	h.Write(data)
+	sign := hex.EncodeToString(h.Sum(nil))
+	fmt.Println(sign)
+
+	Server(apiKey, sign, data)
 }
