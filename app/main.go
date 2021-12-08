737a3e1281f0a059dc5065bc81fc9aaf90b9a6a3
seiya endo <seiya.endo0830@gmail.com>
2021-12-08 18:03:10 +0900

    [update] websocket realtime btc price

--- app/main.go
+++ app/main.go
@@ -1,21 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"github.com/markcheno/go-quote"
-	"github.com/markcheno/go-talib"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/url"
 )
 
+type JsonRPC2 struct {
+	Version string      `json:"jsonrpc"`
+	Method  string      `json:"method"`
+	Params  interface{} `json:"params"`
+	Result  interface{} `json:"result,omitempty"`
+	Id      *int        `json:"id,omitempty"`
+}
+
+type SubscribeParams struct {
+	Channel string `json:"channel"`
+}
+
 func main() {
-	spy, _ := quote.NewQuoteFromYahoo(
-		"spy", "2018-01-01", "2019-01-01", quote.Daily, true)
-	fmt.Print(spy.CSV())
-	// talib ... 株価の計算で使用するパッケージ
-	// rsi ... テクニカルインディケータ（株が売られすぎか、買われすぎかを見る指標）
-	// spy.Close ... 1日の株価の終値
-	rsi2 := talib.Rsi(spy.Close, 2)
-	fmt.Println(rsi2)
-	// Ema ... ムービングアベレージ（株価の平均）の1つ
-	mva := talib.Ema(spy.Close, 14)
-	fmt.Println(mva)
+	u := url.URL{Scheme: "wss", Host: "ws.lightstream.bitflyer.com", Path: "/json-rpc"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer func(c *websocket.Conn) {
+		if err := c.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}(c)
+
+	if err := c.WriteJSON(&JsonRPC2{Version: "2.0", Method: "subscribe", Params: &SubscribeParams{"lightning_ticker_BTC_JPY"}}); err != nil {
+		log.Fatal("subscribe:", err)
+		return
+	}
+
+	for {
+		message := new(JsonRPC2)
+		if err := c.ReadJSON(message); err != nil {
+			log.Println("read:", err)
+			return
+		}
+
+		if message.Method == "channelMessage" {
+			log.Println(message.Params)
+		}
+	}
 }
