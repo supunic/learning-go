540bba86e2970e51ee231187be529525eec31128
seiya endo <seiya.endo0830@gmail.com>
2021-12-08 14:44:35 +0900

    [update] regex

--- app/main.go
+++ app/main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"regexp"
 )
 
 func main() {
-	t := time.Now()
-	fmt.Println(t)
-	fmt.Println(t.Format(time.RFC3339))
-	fmt.Println(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+	match, _ := regexp.MatchString("a([a-z]+)e", "apple")
+	fmt.Println(match)
+
+	r := regexp.MustCompile("a([a-z]+)e")
+	ms := r.MatchString("apple")
+	fmt.Println(ms)
+
+	r2 := regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+	fs := r2.FindString("/view/test")
+	fmt.Println(fs)
+	fss := r2.FindStringSubmatch("/view/test")
+	fmt.Println(fss, fss[0], fss[1], fss[2])
+	fss = r2.FindStringSubmatch("/edit/test")
+	fmt.Println(fss, fss[0], fss[1], fss[2])
+	fss = r2.FindStringSubmatch("/save/test")
+	fmt.Println(fss, fss[0], fss[1], fss[2])
 }
