5f34b5fbe1090dc813ddfff5d2bf5df277935ca5
seiya endo <seiya.endo0830@gmail.com>
2021-12-11 17:10:19 +0900

    [remove] not used code

--- app/main.go
+++ app/main.go
@@ -80,7 +80,6 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 }
 
 func main() {
-	makeHandler(viewHandler)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
